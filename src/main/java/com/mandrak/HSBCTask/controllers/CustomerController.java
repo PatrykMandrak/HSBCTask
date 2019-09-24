3a86b6be68e7a5af55a68fb4cd1a5dbaf65dd7e7
PatrykMandrak <patrykmandrak123@gmail.com>
2019-09-24 02:34:36 +0200

    Implemented CustomerController with handling getting and creating customer

--- src/main/java/com/mandrak/HSBCTask/controllers/CustomerController.java
+++ src/main/java/com/mandrak/HSBCTask/controllers/CustomerController.java
@@ -1,4 +1,62 @@
 package com.mandrak.HSBCTask.controllers;
 
+import com.mandrak.HSBCTask.models.Customer;
+import com.mandrak.HSBCTask.services.CustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
+
+@RequestMapping("/api/customer")
+@RestController
 public class CustomerController {
+    private final CustomerService customerService;
+
+    @Autowired
+    public CustomerController(CustomerService customerService) {
+        this.customerService = customerService;
+    }
+
+    @GetMapping()
+    public ResponseEntity<Iterable<Customer>> getAll() {
+        //notificationService.sendNotification();
+        return ResponseEntity.ok(customerService.getAll());
+    }
+
+    @PostMapping()
+    public ResponseEntity create(@Valid @RequestBody Customer customer) {
+        return ResponseEntity.ok(customerService.save(customer));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Customer> findById(@PathVariable Long id) {
+        Optional<Customer> stock = customerService.findById(id);
+        if (!stock.isPresent()) {
+            ResponseEntity.badRequest().build();
+        }
+
+        return ResponseEntity.ok(stock.get());
+    }
+
+    //Additional methods not included in requirements
+
+/*    @PutMapping("/{id}")
+    public ResponseEntity<Customer> update(@PathVariable Long id, @Valid @RequestBody Customer customer) {
+        if (!customerService.findById(id).isPresent()) {
+            ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(customerService.save(customer));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable Long id) {
+        if (!customerService.findById(id).isPresent()) {
+            ResponseEntity.badRequest().build();
+        }
+        customerService.deleteById(id);
+
+        return ResponseEntity.ok().build();
+    }*/
 }
