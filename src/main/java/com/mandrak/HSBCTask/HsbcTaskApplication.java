77f1365213643959c611e8ef915614d40e246876
PatrykMandrak <patrykmandrak123@gmail.com>
2019-09-24 02:12:51 +0200

    Initial commit with SPRING Initializr dependencies

---
+++ src/main/java/com/mandrak/HSBCTask/HsbcTaskApplication.java
@@ -0,0 +1,13 @@
+package com.mandrak.HSBCTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsbcTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsbcTaskApplication.class, args);
+	}
+
+}
